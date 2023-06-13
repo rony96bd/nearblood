1f720ea384f85289b8b88cb89bd489be36226652
Md. Rakib Uddin <r.rony9966@gmail.com>
2023-06-13 13:34:51 +0600

    Update_footer

--- core/resources/views/templates/basic/partials/footer.blade.php
+++ core/resources/views/templates/basic/partials/footer.blade.php
@@ -87,7 +87,8 @@
                     <div class="footer-widget">
                         <ul class="footer-overview-list text-end">
                             <li class="footer-overview">
-                                <h4 class="footer-overview__number">{{__($city)}}, {{__($footer->data_values->first_count_digits)}}</h4>
+                                <h4 class="footer-overview__number">{{__($city)}}</h4>
+                                {{-- {{__($footer->data_values->first_count_digits)}} --}}
                                 <p class="footer-overview__caption">{{__($footer->data_values->first_count_title)}}</p>
                             </li>
                             {{-- <li class="footer-overview">
@@ -99,6 +100,7 @@
                 </div>
             </div>
         </div>
+
     </div>
     <div class="footer__bottom">
         <div class="container">
