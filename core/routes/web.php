5c36078ea3e8941af76cb3e45211eb6f26010717
Md. Rakib Uddin <r.rony9966@gmail.com>
2023-07-04 20:41:15 +0600

    fix3.1

--- core/routes/web.php
+++ core/routes/web.php
@@ -244,6 +244,6 @@ Route::get('/menu/{slug}/{id}', 'SiteController@footerMenu')->name('footer.menu'
 Route::get('/add/{id}', 'SiteController@adclicked')->name('add.clicked');
 Route::post('/subscribe', 'SiteController@subscribe')->name('subscribe');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', 'SiteController@index')->name('home');
 
 Route::get('/verify', 'SiteController@verifyDonor')->name('verify.donor');
