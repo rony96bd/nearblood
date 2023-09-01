81bcce3e18a39de4815fffa1903983cee201ddda
Md. Rakib Uddin <r.rony9966@gmail.com>
2023-09-01 08:24:01 +0600

    check5

--- core/resources/views/templates/basic/donor_details.blade.php
+++ core/resources/views/templates/basic/donor_details.blade.php
@@ -101,8 +101,9 @@
                         </li>
 
                         <li>
+                            <span class="caption">@lang('Phone')</span>
                             @if (auth()->guard('donor')->check())
-                                <span class="caption">@lang('Phone')</span>
+
                                 <span class="value">{{ __($donor->phone) }} <a
                                         href="https://wa.me/+88{{ __($donor->phone) }}"> Click to chat on
                                         WhatsApp</a></span>
