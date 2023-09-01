7ac510186e059a2d7e7009db9b006132b40d56b6
Md. Rakib Uddin <r.rony9966@gmail.com>
2023-09-01 08:52:55 +0600

    test

--- core/resources/views/templates/basic/partials/header.blade.php
+++ core/resources/views/templates/basic/partials/header.blade.php
@@ -29,6 +29,7 @@
                 <a class="site-logo site-title" href="{{route('home')}}">
                     <img src="{{getImage(imagePath()['logoIcon']['path'] .'/logo.png')}}" alt="@lang('logo')">
                 </a>
+                hello
                 <button onclick="window.location.href='https://nearblood.com/apply/donor';" class="navbar-toggler ms-auto text-white" style="font-size: 15px;border: solid #FB3640 1px;border-radius: 5px;background-color: #FB3640;padding: 5px 3px 5px 3px;" type="button" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     Apply As Donor
                 </button>
