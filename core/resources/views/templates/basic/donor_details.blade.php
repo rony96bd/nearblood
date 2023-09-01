6b39ce68e22112edae377e7beb63effe3daf74cf
Md. Rakib Uddin <r.rony9966@gmail.com>
2023-09-01 09:32:18 +0600

    All Done

--- core/resources/views/templates/basic/donor_details.blade.php
+++ core/resources/views/templates/basic/donor_details.blade.php
@@ -108,12 +108,9 @@
                                         href="https://wa.me/+88{{ __($donor->phone) }}"> Click to chat on
                                         WhatsApp</a></span>
                             @else
-                                <span class="value">01xxxxxxxxx <a href="https://nearblood.com/apply/donor"> Click to signup
-                                        or
-                                        login</a></span>
+                                <span class="value">01xxxxxxxxx <a href="https://nearblood.com/apply/donor"> See Phone Number? Please Signup and Login</a></span>
                             @endif
                         </li>
-
 
                         <li>
                             <span class="caption">@lang('Profession')</span>
