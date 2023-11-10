adf7af9ae1756d2b4c274b53cfa85f21c9a473ec
Md. Rakib Uddin <r.rony9966@gmail.com>
2023-11-11 01:22:48 +0600

    upd blog

--- core/resources/views/templates/basic/sections/blog.blade.php
+++ core/resources/views/templates/basic/sections/blog.blade.php
@@ -7,36 +7,41 @@
         <div class="row justify-content-center">
             <div class="col-xxl-6 col-xl-8 col-lg-10">
                 <div class="section-header text-center">
-                    <h2 class="section-title">{{__($blog->data_values->heading)}}</h2>
-                    <p class="mt-2">{{__($blog->data_values->sub_heading)}}</p>
+                    <h2 class="section-title">{{ __($blog->data_values->heading) }}</h2>
+                    <p class="mt-2">{{ __($blog->data_values->sub_heading) }}</p>
                 </div>
             </div>
         </div>
 
         <div class="row gy-4 justify-content-center">
-            @foreach($blogElements as $blogElement)
+            @foreach ($blogElements as $blogElement)
                 <div class="col-lg-4 col-md-6">
                     <div class="blog-post">
                         <div class="blog-post__thumb">
-                            <a href="{{ route('blog.details',[$blogElement->id,slug($blogElement->data_values->title)]) }}" class="d-block"><img src="{{getImage('assets/images/frontend/blog/'. @$blogElement->data_values->blog_image, '728x465')}}" alt="@lang('image1')"></a>
+                            <a href="{{ route('blog.details', [$blogElement->id, slug($blogElement->data_values->title)]) }}"
+                                class="d-block"><img
+                                    src="{{ getImage('assets/images/frontend/blog/' . @$blogElement->data_values->blog_image, '728x465') }}"
+                                    alt="@lang('image1')"></a>
                         </div>
                         <div class="blog-post__content">
                             <ul class="blog-post__meta">
-                                <li>{{showDateTime($blogElement->created_at, 'd M Y')}}</li>
+                                <li>{{ showDateTime($blogElement->created_at, 'd M Y') }}</li>
                             </ul>
-                            <h5 class="blog-post__title mt-3"><a href="{{ route('blog.details',[$blogElement->id,slug($blogElement->data_values->title)]) }}">{{__($blogElement->data_values->title)}}</a></h5>
+                            <h5 class="blog-post__title mt-3"><a
+                                    href="{{ route('blog.details', [$blogElement->id, slug($blogElement->data_values->title)]) }}">{{ __($blogElement->data_values->title) }}</a>
+                            </h5>
                         </div>
                     </div>
                 </div>
             @endforeach
         </div>
     </div>
-    <div style="width: 100%; padding-top: 20px" class="text-center"><span><a href="{{ route('blog') }}"
-        class="custom-btn2" style="
-        background: #FB3640;
-        border-radius: 5px;
-        padding: 4px 10px 3px 10px;
-        color: white;
-        font-weight: bold;
-    ">সকল পোস্ট</a></span></div>
+    <div style="width: 100%; padding-top: 20px" class="text-center">
+        <span>
+            <a href="{{ route('blog') }}" class="custom-btn2"
+                style="background: #FB3640; border-radius: 5px; padding: 4px 10px 3px 10px; color: white; font-weight: bold;">
+                সকল পোস্ট
+            </a>
+        </span>
+    </div>
 </section>
